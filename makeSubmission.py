3c90a222104dd14f978c1d33e889a5fffea44311
lexual <lex@jbadigital.com>
2012-11-09 13:46:24 +1100

    Use pandas over numpy's csv parsing for performance: http://wesmckinney.com/blog/?p=543

--- makeSubmission.py
+++ makeSubmission.py
@@ -1,20 +1,25 @@
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from numpy import genfromtxt, savetxt
+
 
 def main():
-    #create the training & test sets, skipping the header row with [1:]
-    dataset = genfromtxt(open('Data/train.csv','r'), delimiter=',', dtype='f8')[1:]    
-    target = [x[0] for x in dataset]
-    train = [x[1:] for x in dataset]
-    test = genfromtxt(open('Data/test.csv','r'), delimiter=',', dtype='f8')[1:]
-    
-    #create and train the random forest
-    #multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
-    rf = RandomForestClassifier(n_estimators=100)
+    # create the training & test sets
+    dataset = pd.read_csv('Data/train.csv')
+
+    target = dataset.Activity.values
+    train = dataset.drop('Activity', axis=1).values
+
+    test = pd.read_csv('Data/test.csv').values
+
+    # create and train the random forest
+    # n_jobs set to -1 will use the number of cores present on your system.
+    rf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
     rf.fit(train, target)
     predicted_probs = [x[1] for x in rf.predict_proba(test)]
+    predicted_probs = pd.Series(predicted_probs)
 
-    savetxt('Data/submission.csv', predicted_probs, delimiter=',', fmt='%f')
+    predicted_probs.to_csv('Data/submission.csv', index=False,
+                            float_format="%f")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
